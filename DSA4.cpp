d1a71c118f9a1be27057850def82478c81615f5b
anirudha225 <rrao48089@gmail.com>
2025-10-05 00:05:23 +0530

    Painters partition problem

--- DSA4.cpp
+++ DSA4.cpp
@@ -1,26 +1,65 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 using namespace std;
 
+vector <int> arr = {40,30,10,20};
+int n=arr.size(), m=2, st, end, mid;
+
+
+// To find max 
+int maxOf (vector <int> &a){
+  int ans = INT_MIN;
+  for(int i=0 ; i<a.size() ; i++){
+    ans = max(ans,a[i]);
+  }
+return ans;
+}
+
+// To find sum of the array
+int sum (vector <int> &s){
+  int sum1 = 0;
+  for(int i=0 ; i<s.size() ; i++){
+    sum1 += s[i];
+  }
+return sum1;
+}
+
+// To check whether the mid lies in possible solution or not
+
+bool isPos (int maxTime){
+  int painter = 1, time =0;
+  for(int i=0 ; i<n ; i++){
+    if(time + arr[i] <= maxTime){
+      time += arr[i];
+    }
+    else{
+      painter++;
+      time = arr[i];
+    }
+  }
+  return(painter <= m);
+    
+}
+
+// Binary serach
 int main(){
 
-  vector <int> arr = {40,30,10,20};
-  vector <int> ans;
-  int p1=0, p2, fans=INT_MAX;
-  
-  for(int i=0 ; i<arr.size()-1; i++){
+  st = maxOf(arr);
+  end = sum(arr);
 
-    p1 += arr[i];
-    p2 = 100-p1;
-    ans.push_back(max(p1,p2));
+  while(st <= end){
+    mid = st + (end-st)/2;
+
+    if(isPos(mid))
+      end = mid - 1;
+    else
+      st = mid + 1; 
 
   }
 
-  for(int i=0 ; i<ans.size()-1 ; i++){
-    fans = min(fans,ans[i]);
-  }
-
-  cout << fans;
+  cout << st;
 return 0;
 
 }
+
